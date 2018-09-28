9cc17ae2d91ce91aa8cb99b1f5486fe40ec47f4d
JackGeminiKu <JackGeminiKu@qq.com>
2018-09-28 17:48:04 +0800

    添加笔记

--- public/static/jack/collapse/collapse.js
+++ public/static/jack/collapse/collapse.js
@@ -1,69 +1,41 @@
-/**
-数字
-<div class="list list-number">
-    <div class="title">XXX</div>
-    <div class="content open">
-        ...
-    </div>
-
-    <div class="title">XXX</div>
-    <div class="content closed">
-        ...
-    </div>
-</div>
-
-方块
-<div class="list list-square">
-    <div class="title">XXX</div>
-    <div class="content open">
-        ...
-    </div>
-
-    <div class="title">XXX</div>
-    <div class="content closed">
-        ...
-    </div>
-</div>
-**/
-
 // 折叠&展开初始化
 $(function () {
-    $("div.list").each(function (index) {
-        // 添加序号或图标
-        var classes = $(this).attr("class").split(" ");
-        if ($.inArray("list-number", classes) != -1) {  // 数字
-            $(this).children("div.title").each(function (index) {
-                $(this).text(++index + ". " + $(this).text());
-            })
-        } else if ($.inArray("list-square", classes) != -1) {   // 方块
-            $(this).children("div.title").each(function (index) {
-                $(this).text("￭ " + $(this).text());
-            })
-        } else if ($.inArray("list-none", classes) != -1) {   // 无
-            // 不需要添加数组或方块
-        }
+	$("div.list").each(function (index) {
+		// 添加序号或图标
+		var classes = $(this).attr("class").split(" ");
+		if ($.inArray("list-number", classes) != -1) {  // 数字
+			$(this).children("div.title").each(function (index) {
+				$(this).text(++index + ". " + $(this).text());
+			})
+		} else if ($.inArray("list-square", classes) != -1) {   // 方块
+			$(this).children("div.title").each(function (index) {
+				$(this).text("￭ " + $(this).text());
+			})
+		} else if ($.inArray("list-none", classes) != -1) {   // 无
+			// 不需要添加数组或方块
+		}
 
-        // 初始状态
-        $(this).children("div.content").each(function (index) {
-            if ($(this).attr("class").indexOf("closed") != -1) {
-                $(this).css("display", "none");
-            }
-        })
-    })
+		// 初始状态
+		$(this).children("div.content").each(function (index) {
+			if ($(this).attr("class").indexOf("closed") != -1) {
+				$(this).css("display", "none");
+			}
+		})
+	})
 })
 
 // 点击title时, 折叠|展开content
 $(function () {
-    $(document).click(function (e) {
-        var currentElement = $(e.target);
-        var classes = currentElement.attr("class").split(" ");
-        if ($.inArray("title", classes) != -1) {
-            var nextSiblin = currentElement.next();
-            classes = nextSiblin.attr("class").split(" ");
-            if ($.inArray("content", classes) != -1) {
-                nextSiblin.css("display", nextSiblin.css("display") == "none" ? "block" : "none");
-                return;
-            }
-        }
-    })
+	$(document).click(function (e) {
+		var currentElement = $(e.target);
+		var classes = currentElement.attr("class").split(" ");
+		if ($.inArray("title", classes) != -1) {
+			var nextSiblin = currentElement.next();
+			classes = nextSiblin.attr("class").split(" ");
+			if ($.inArray("content", classes) != -1) {
+				nextSiblin.css("display", nextSiblin.css("display") == "none" ? "block" : "none");
+				return;
+			}
+		}
+	})
 })
