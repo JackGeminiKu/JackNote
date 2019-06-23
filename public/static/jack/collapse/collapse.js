cc97e080c16be0e87b82357918958a272b12b9d9
JackGeminiKU <JackGeminiKu@gmail.com>
2019-06-23 21:56:58 +0800

    Update collapse.js

--- public/static/jack/collapse/collapse.js
+++ public/static/jack/collapse/collapse.js
@@ -1,30 +1,30 @@
 // 折叠&展开初始化
 $(function () {
-	var divList = $("div.list")[0];
-	addListNumber("", divList);
-	showOutline(divList);
-//	$("div.list").each(function (index) {
-//		// 添加序号或图标
-//		var classes = $(this).attr("class").split(" ");
-//		if ($.inArray("list-number", classes) != -1) {  // 数字
-//			$(this).children("div.title").each(function (index) {
-//				$(this).text(++index + ". " + $(this).text());
-//			})
-//		} else if ($.inArray("list-square", classes) != -1) {   // 方块
-//			$(this).children("div.title").each(function (index) {
-//				$(this).text("￭ " + $(this).text());
-//			})
-//		} else if ($.inArray("list-none", classes) != -1) {   // 无
-//			// 不需要添加数组或方块
-//		}
-//
-//		// 初始状态
-//		$(this).children("div.content").each(function (index) {
-//			if ($(this).attr("class").indexOf("closed") != -1) {
-//				$(this).css("display", "none");
-//			}
-//		})
-//	})
+//	var divList = $("div.list")[0];
+//	addListNumber("", divList);
+//	showOutline(divList);
+	$("div.list").each(function (index) {
+		// 添加序号或图标
+		var classes = $(this).attr("class").split(" ");
+		if ($.inArray("list-number", classes) != -1) {  // 数字
+			$(this).children("div.title").each(function (index) {
+				$(this).text(++index + ". " + $(this).text());
+			})
+		} else if ($.inArray("list-square", classes) != -1) {   // 方块
+			$(this).children("div.title").each(function (index) {
+				$(this).text("￭ " + $(this).text());
+			})
+		} else if ($.inArray("list-none", classes) != -1) {   // 无
+			// 不需要添加数组或方块
+		}
+
+		// 初始状态
+		$(this).children("div.content").each(function (index) {
+			if ($(this).attr("class").indexOf("closed") != -1) {
+				$(this).css("display", "none");
+			}
+		})
+	})
 })
 
 // 点击title时, 折叠|展开content
